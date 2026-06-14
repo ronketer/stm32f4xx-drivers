7e149486fc73cc64197af5ad781881019b222e20
Ron Keter <ron.keter@mail.huji.ac.il>
2026-06-14 17:52:23 +0300

    feat(gpio): add base memory addresses for flash and sram

--- drivers/Inc/stm32f405xx.h
+++ drivers/Inc/stm32f405xx.h
@@ -9,4 +9,18 @@
 #define INC_STM32F405XX_H_
 
 
+// base addresses of flash and SRAM memories
+
+
+// U tells the compiler this are unsigned ( make sense since this are memory locations)
+#define FLASH_BASEADDR							0x08000000U
+#define SRAM1_BASEADDR							0x20000000U
+#define SRAM2_BASEADDR							0x2001C000U
+#define ROM										0x1FFF0000U
+
+#define SRAM 									SRAM1_BASEADDR
+
+
+
+
 #endif /* INC_STM32F405XX_H_ */
